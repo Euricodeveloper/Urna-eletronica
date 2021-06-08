335cb331be5f810bd796d15fd10446b24b88977f
Eurico Oliveira <eurico.farias@icloud.com>
2021-06-08 08:40:33 -0300

    urna

---
+++ etapas.js
@@ -0,0 +1,59 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+            numero: '38111',
+            nome: 'Bozo',
+            partido: 'Milicia',
+            fotos:[
+            {url: './bolsonaro.jfif', legenda: 'Vereador'}
+            ]
+            },
+            {
+                numero: '38222',
+                nome: 'Luladrao',
+                partido: 'Papuda',
+                fotos:[
+                {url: './images/lula.jfif', legenda: 'Vereador'}
+                ]
+                },
+            
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+            numero: '77',
+            nome: 'Daciolo',
+            partido: 'Gloria a Deux',
+            fotos:[
+            {url: './images/daciolo.jpg', legenda: 'Prefeito'},
+            {url: './images/marina.jpg', legenda: 'Vice-Prefeito', small: true},
+            ]
+            },
+            
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+            numero: '66',
+            nome: 'Trumpilantra',
+            partido: 'EUA',
+            fotos:[
+            {url: './images/trump.jfif', legenda: 'Prefeito'},
+            {url: './images/coringa.jpg', legenda: 'Vice-Prefeito', small: true},
+            ]
+            },
+            
+        ]
+    },
+]
